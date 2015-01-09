b6b0674a20e5697e1786dd7607af04a3c247fa25
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-01-09 16:48:26 +0600

    Local settings added.

--- chelgradirni/settings.py
+++ chelgradirni/settings.py
@@ -60,20 +60,6 @@ ROOT_URLCONF = 'chelgradirni.urls'
 WSGI_APPLICATION = 'chelgradirni.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chelgradirni.ru',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
