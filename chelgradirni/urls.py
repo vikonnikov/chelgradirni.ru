995ce2da6019ac28d503638f3d86cbf6511ecf33
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-01-09 16:59:28 +0600

    Url patters corrected.

--- chelgradirni/urls.py
+++ chelgradirni/urls.py
@@ -16,6 +16,6 @@ urlpatterns += patterns('django.contrib.flatpages.views',
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = patterns('',
+urlpatterns = +patterns('',
     # ... the rest of your URLconf goes here ...
     ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
