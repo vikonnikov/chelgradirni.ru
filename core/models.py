f3169fd759e4868b50d83f0c52d71152249e74c9
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-01-10 16:28:05 +0600

    __str__() method added to Section model.

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class Section(models.Model):
     title = models.CharField(max_length=100)
     slug = models.CharField(max_length=100)
     position = models.IntegerField()
+
+    def __str__(self):
+        return self.title
