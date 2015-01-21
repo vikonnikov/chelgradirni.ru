12153318f4666df483c49e293571f430a87b3b03
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-01-22 03:57:28 +0500

    Models MenuItem and Page added to admin interface.

--- core/admin.py
+++ core/admin.py
@@ -3,13 +3,21 @@ from core.models import Section, Keyword, Page, MenuItem
 
 # Register your models here.
 
-class SectionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'position')
+# class SectionAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'position')
+#     ordering =  ('position',)
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'position')
     ordering =  ('position',)
 
-admin.site.register(Section, SectionAdmin)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url')
+    ordering =  ('title',)
+
+# admin.site.register(Section, SectionAdmin)
 
 admin.site.register(Keyword)
-admin.site.register(Page)
-admin.site.register(MenuItem)
+admin.site.register(Page, PageAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
 
